1c49b86f0a7cb3b0e2f115b32ed178ef9c7081e2
Alexandr123000 <alexandrt10000@gmail.com>
2026-01-07 21:17:20 +0300

    EvenNumber.cpp

---
+++ EvenNumber.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 10;
+    
+    if (number % 2 == 0) // if a number is even
+    {
+        cout << "The number is even." << endl;
+    }
+    else // if a number is not even
+    {
+        cout << "The number is odd." << endl;
+    }
+    
+    return 0;
+}
