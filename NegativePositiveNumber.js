1dcfd0f8731cd9899bafd9384f440291a4bdc426
Alexandr <alexandrt10000@gmail.com>
2026-06-13 16:25:42 +0300

    NegativePositiveNumber.js

---
+++ NegativePositiveNumber.js
@@ -0,0 +1,13 @@
+var number = 5;
+if (number < 0)
+{
+    console.log("The number is negative.");
+}
+else if (number > 0)
+{
+    console.log("The number is positive.");
+}
+else
+{
+    console.log("The number is neither negative nor positive.");
+}
