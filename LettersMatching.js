6a2111e9d4136f49a29c0c627b2cd447237e7e40
Alexandr <alexandrt10000@gmail.com>
2026-06-13 19:51:36 +0300

    LettersMatching.js

---
+++ LettersMatching.js
@@ -0,0 +1,10 @@
+let firstWord = "first";
+let secondWord = "second";
+if (firstWord[firstWord.length-1] == secondWord[0])
+{
+  console.log("The last letter of the first word matches the first letter of the second word.");
+}
+else
+{
+  console.log("The last letter of the first word does not match the first letter of the second word.");
+}
