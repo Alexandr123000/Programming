7eea4e6b3426ef6df67420a137735b6490cf74e4
Alexandr <alexandrt10000@gmail.com>
2026-06-20 20:54:45 +0300

    CapitalizedFirstCharacter.js

---
+++ CapitalizedFirstCharacter.js
@@ -0,0 +1,7 @@
+let line = "this is a line.";
+
+console.log("A line: " + line);
+
+line = line.charAt(0).toUpperCase() + line.slice(1); //first character change
+
+console.log("The line changed: " + line);
